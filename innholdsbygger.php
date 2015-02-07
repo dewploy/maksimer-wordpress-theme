833bbf9f09e2ca67fcebd5b8cea665540ad2d9bd
Ronny Myhre Njaaastad <ronny@njaastad.no>
2015-02-07 11:20:06 +0100

    Implemented use of transients for innholdsbygger

--- innholdsbygger.php
+++ innholdsbygger.php
@@ -2,16 +2,7 @@
 	$innholdsbygger = get_field( 'innholdsbygger' );
 	if ( function_exists ( 'get_row_layout' ) && !empty( $innholdsbygger ) ) :
 
-		global $innholdsbygger_teller;
-		$innholdsbygger_teller = 1;
-
-		while ( has_sub_field( 'innholdsbygger' ) ) :
-
-			get_template_part( 'innholdsbygger', get_row_layout() );
-
-			$innholdsbygger_teller++;
-
-		endwhile;
+		echo get_innhold( $innholdsbygger, get_the_ID() );
 
 	else :
 ?>
@@ -22,4 +13,36 @@
 
 <?php
 	endif;
+?>
+
+
+<?php 
+
+
+
+function get_innhold ( $innholdsbygger, $id ) {
+	
+	$transient = get_transient( sanitize_key( 'innholdsbygger-'.$id ) );
+	if ( !is_user_logged_in() && $transient ) {
+		return $transient;
+	}	
+	
+	
+	global $innholdsbygger_teller;
+	$innholdsbygger_teller = 1;
+	
+	ob_start();
+	while ( has_sub_field( 'innholdsbygger' ) ) {
+		get_template_part( 'innholdsbygger', get_row_layout() );
+		$innholdsbygger_teller++;
+	}
+	$innholdet =  ob_get_clean();
+	
+	if ( !is_user_logged_in() ) {
+		set_transient( sanitize_key( 'innholdsbygger-'.$id ), $innholdet, HOUR_IN_SECONDS );
+	}
+	return $innholdet;
+	
+}
+
 ?>
